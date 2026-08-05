e9bb2e5a7d430446a4cd45f263fc29846ab565ba
Lam Nguyen <lamtnguyen989@gmail.com>
2026-08-04 19:09:20 -0500

    finally got things to compile

--- build-cuda-via-cli.sh
+++ build-cuda-via-cli.sh
@@ -1,9 +1,11 @@
+CUDA_VER=13.0.3
+CUDA_COMPUTE_CAPABILITIES=12.0
+
 bazel build -c opt --config=cuda \
   --repo_env TF_NEED_CUDA=1 \
   --repo_env TF_NEED_ROCM=0 \
-  --action_env CUDA_TOOLKIT_PATH="/usr/local/cuda-13.3" \
-  --action_env TF_CUDA_VERSION="13.0.3" \
-  --action_env TF_CUDA_COMPUTE_CAPABILITIES="12.0" \
+  --action_env TF_CUDA_VERSION="$CUDA_VER" \
+  --action_env TF_CUDA_COMPUTE_CAPABILITIES="$CUDA_COMPUTE_CAPABILITIES" \
   --define=using_rocm=false \
   --incompatible_disallow_empty_glob=false \
   //:xla_compute
