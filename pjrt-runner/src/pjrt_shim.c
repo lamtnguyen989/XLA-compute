351b9030adb5afa468660de0a8e6c2be9408446b
Lam Nguyen <lamtnguyen989@gmail.com>
2026-08-15 12:12:57 -0500

    PJRT runner C API

---
+++ pjrt-runner/src/pjrt_shim.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <dlfcn.h>
+
+#include "xla/pjrt/c/pjrt_c_api.h"
+
+// Define the input dimension
+#define BATCH_SIZE 32
+#define N 88200
+
+
+int main(int argc, char** argv)
+{
+
+}
