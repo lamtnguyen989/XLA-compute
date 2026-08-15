b6c97f5a6bf0e401f1cad655c1b5dac441f2a010
Lam Nguyen <lamtnguyen989@gmail.com>
2026-08-15 14:02:00 -0500

    getting API and clients

--- pjrt-runner/src/pjrt_runner.c
+++ pjrt-runner/src/pjrt_runner.c
@@ -3,6 +3,7 @@
 ***/
 
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <dlfcn.h>
 
@@ -13,6 +14,7 @@
 #define BATCH_SIZE 32
 #define N 88200
 
+// TODO: Error checking macros
 
 int main(int argc, char** argv)
 {
@@ -25,4 +27,28 @@ int main(int argc, char** argv)
     const char* pjrt_path = argv[2];
 
     // Load the PJRT plugin
+    void* pjrt_handle = dlopen(pjrt_path, RTLD_NOW | RTLD_LOCAL);
+    if (pjrt_handle == NULL) {
+        fprintf(stderr, "Failed to open the PJRT C API: %s\n", dlerror());
+        return 2;
+    }
+
+    typedef const PJRT_Api *(*GetPjrtApiFn)(void);
+    GetPjrtApiFn get_api = (GetPjrtApiFn)dlsym(pjrt_handle, "GetPjrtApi");
+    if (!get_api) {
+        fprintf(stderr, "Could not resolve GetPjrtApi: %s\n", dlerror());
+        return 3;
+    }
+    const PJRT_Api *api = get_api();
+    printf("Loaded PJRT plugin, API version %d.%d\n", 
+            api->pjrt_api_version.major_version,
+            api->pjrt_api_version.minor_version);
+
+    // Create a client
+    PJRT_Client_Create_Args client_args;
+    memset(&client_args, 0, sizeof(client_args));
+    client_args.struct_size = PJRT_Client_Create_Args_STRUCT_SIZE;
+    PJRT_Client* client = client_args.client;
+
+    // Read and load MLIR/StableHLO module
 }
